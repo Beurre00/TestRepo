4dffaa96b750c51162444a6658a009293a342338
Beurre00 <al_al_pl@yahoo.co.uk>
2014-09-05 00:32:17 +0200

    Adding files

---
+++ proj.android/obj/local/armeabi/objs/cocosdenshion_static/jni/cddandroidAndroidJavaEngine.o.d
@@ -0,0 +1,62 @@
+obj/local/armeabi/objs/cocosdenshion_static/jni/cddandroidAndroidJavaEngine.o: \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/jni/cddandroidAndroidJavaEngine.cpp \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/jni/cddandroidAndroidJavaEngine.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../include/SimpleAudioEngine.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../include/Export.h \
+ D:/AndroidGames/ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/typeinfo \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/ctype.h \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/string.h \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/malloc.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../2d/platform/android/jni/JniHelper.h \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/jni.h \
+ D:/AndroidGames/ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../base/CCPlatformMacros.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../2d/ccConfig.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../base/CCPlatformConfig.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../base/CCPlatformConfig.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../2d/platform/android/CCPlatformDefine.h \
+ D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/android/log.h \
+ D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/ccdandroidUtils.h
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/jni/cddandroidAndroidJavaEngine.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../include/SimpleAudioEngine.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../include/Export.h:
+
+D:/AndroidGames/ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/typeinfo:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/ctype.h:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/string.h:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../2d/platform/android/jni/JniHelper.h:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/jni.h:
+
+D:/AndroidGames/ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../base/CCPlatformMacros.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../2d/ccConfig.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../base/CCPlatformConfig.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../base/CCPlatformConfig.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/../../2d/platform/android/CCPlatformDefine.h:
+
+D:/AndroidGames/ndk/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+D:\AndroidGames\Projects\Test\proj.android\../cocos2d/cocos/audio/android/ccdandroidUtils.h:
